85bc0e00c5d49881083dc9ae66bc51ca9a80c34f
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-10 09:48:33 -0600

    more file structure

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES              ("Lamb Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES              ("Denver Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES              ("Croque Burger", false);
